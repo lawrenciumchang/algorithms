02409406d7270c7af1a038c3d2e6e93acb1a1f2a
Lawrence Chang <lchang@credera.com>
2017-08-16 11:22:02 -0500

    added table view to permutations page

---
+++ Algorithms/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  Algorithms
+//
+//  Created by Lawrence Chang on 8/16/17.
+//  Copyright © 2017 Lawrencium. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var permutationImage: UIImageView!
+    @IBOutlet weak var solutionLabel: UILabel!
+
+}
--- Algorithms/PermutationsViewController.swift
+++ Algorithms/PermutationsViewController.swift
@@ -8,10 +8,35 @@
 
 import UIKit
 
-class PermutationsViewController: UIViewController {
+class PermutationsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var permutationsTable: UITableView!
+    
+    var dataList:[(String,String)] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dataList.append(("p1.gif","R² U F B' R² F' B U R²"))
+        dataList.append(("p1b.gif", "R² U' F B' R² F' B U' R²"))
+        permutationsTable.delegate = self
+        permutationsTable.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataList.count
+    }
+    
+    func tableView(_ permutationsTable: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = permutationsTable.dequeueReusableCell(withIdentifier: "CustomCell") as! CustomTableViewCell
+        cell.permutationImage.image = UIImage(named: dataList[indexPath.row].0)
+        cell.solutionLabel.text = dataList[indexPath.row].1
+        return cell
     }
     
 }
