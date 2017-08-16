9e4a6165fd79ac619178d7df1b1e783748465be2
Lawrence Chang <lchang@credera.com>
2017-08-16 16:32:20 -0500

    added button to f2l page

--- Algorithms/F2LViewController.swift
+++ Algorithms/F2LViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class F2LViewController: UIViewController {
     
+    @IBOutlet weak var algorithmsBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        algorithmsBtn.layer.cornerRadius = 4
+    }
+    
+    @IBAction func onBtnClick(_ sender: Any) {
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+        if let url = URL(string: "http://www.speedcubing.com/f2l_crossbottom.html") {
+            UIApplication.shared.open(url, options: [:])
+        }
     }
     
 }
