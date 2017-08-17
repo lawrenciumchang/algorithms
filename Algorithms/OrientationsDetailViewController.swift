72b45b99421ef2a8929800cf30b1c22cb67ceeb4
Lawrence Chang <lchang@credera.com>
2017-08-17 16:15:06 -0500

    added components to detail pages

--- Algorithms/OrientationsDetailViewController.swift
+++ Algorithms/OrientationsDetailViewController.swift
@@ -12,16 +12,18 @@ class OrientationsDetailViewController: UIViewController {
     
     var identifier: Int = 0
     
-    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var cubeImage: UIImageView!
+    @IBOutlet weak var solutionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        cubeImage.image = UIImage(named: Orientations.dataList[identifier].0)
+        solutionLabel.text = Orientations.dataList[identifier].1
+        
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(performSegueToReturnBack))
         swipeRight.direction = .right
         self.view.addGestureRecognizer(swipeRight)
-        
-        testLabel.text = String(identifier)
     }
     
     func performSegueToReturnBack()  {
