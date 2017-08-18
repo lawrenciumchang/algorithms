22f1418d62619f588f3f100caeed22a5c05df3e9
Lawrence Chang <lchang@credera.com>
2017-08-18 15:45:49 -0500

    added glowing animation to timer button when running

--- Algorithms/TimerViewController.swift
+++ Algorithms/TimerViewController.swift
@@ -36,19 +36,31 @@ class TimerViewController: UIViewController {
         panel3.layer.cornerRadius = 4
     }
     
-    func start() {
+    @IBAction func timerBtn1Released(_ sender: UIButton) {
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+        if(isTimerRunning) {
+            stopTimer()
+            stopAnimation()
+        } else {
+            resetTimer()
+            startTimer()
+            startAnimation()
+        }
+    }
+    
+    func startTimer() {
         startTime = Date().timeIntervalSinceReferenceDate - elapsed
         timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         isTimerRunning = true
     }
     
-    func stop() {
+    func stopTimer() {
         elapsed = Date().timeIntervalSinceReferenceDate - startTime
         timer?.invalidate()
         isTimerRunning = false
     }
     
-    func reset() {
+    func resetTimer() {
         timer?.invalidate()
         startTime = 0
         time = 0
@@ -81,27 +93,19 @@ class TimerViewController: UIViewController {
         labelMillisecond.text = strMilliseconds
     }
     
-    func getColorByHex(rgbHexValue:UInt32, alpha:Double = 1.0) -> UIColor {
-        let red = Double((rgbHexValue & 0xFF0000) >> 16) / 256.0
-        let green = Double((rgbHexValue & 0xFF00) >> 8) / 256.0
-        let blue = Double((rgbHexValue & 0xFF)) / 256.0
-        
-        return UIColor(red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(alpha))
+    func startAnimation() {
+        let pulseAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+        pulseAnimation.duration = 1
+        pulseAnimation.fromValue = 1
+        pulseAnimation.toValue = 0.85
+        pulseAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        pulseAnimation.autoreverses = true
+        pulseAnimation.repeatCount = .greatestFiniteMagnitude
+        timerBtn1.layer.add(pulseAnimation, forKey: "animateOpacity")
     }
     
-    @IBAction func timerBtn1Touched(_ sender: UIButton) {
-        timerBtn1.backgroundColor = getColorByHex(rgbHexValue: 0x6375e5, alpha: 1)
-    }
-    
-    @IBAction func timerBtn1Released(_ sender: UIButton) {
-        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-        timerBtn1.backgroundColor = getColorByHex(rgbHexValue: 0x6E82FF, alpha: 1)
-        if (isTimerRunning) {
-            stop()
-        } else {
-            reset()
-            start()
-        }
+    func stopAnimation() {
+        timerBtn1.layer.removeAllAnimations()
     }
     
 }
